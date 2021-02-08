0dea37c626ec8430b0477d7f6f8172e9d5f40fe0
MarcoVercosa <marco2007sky@hotmail.com>
2021-02-08 02:21:15 -0300

    Montagem formulario Ediçao em andamento !

--- client/src/components/painelAdministrativo/listaAnuncios/listaAnuncios.jsx
+++ client/src/components/painelAdministrativo/listaAnuncios/listaAnuncios.jsx
@@ -8,18 +8,71 @@ import SimpleModal from "./modalExibirAnuncio"
 
 export default function ListarAnuncios() {
 
-    const [dadosListaAnuncio, setDadosListaAnuncio] = useState(false)
+    const [dadosListaAnuncio, setDadosListaAnuncio] = useState({
+        todasPaginas: false,
+        paginacao: 0,
+        paginaAvanca: 25,
+        paginaRetorna: 0,
+        paginaAtual: false,
+        paginaFinal: false
+
+    })
 
     useEffect(async () => {
         const classBuscaBD = new BuscaBD()
         let resultado = await classBuscaBD.ListarAnuncios()
-        setDadosListaAnuncio(resultado)
+        setDadosListaAnuncio(prevState => {
+            let calculaTotalPaginas = 0
+            if (resultado.data.length % 25 === 0) { calculaTotalPaginas = Math.floor(resultado.data.length / 25) } else { calculaTotalPaginas = Math.floor(resultado.data.length / 25 + 1) }
+            return {
+                ...prevState, todasPaginas: resultado, paginacao: resultado.data.slice(
+                    dadosListaAnuncio.paginaRetorna, dadosListaAnuncio.paginaAvanca),
+                paginaAtual: 1,
+                paginaFinal: calculaTotalPaginas
+            }
+
+        })
     }, [])
+
+    function FunctionPaginacao(click, direcao) {
+
+        if (direcao === "avancar") {
+            if (dadosListaAnuncio.paginaAvanca >= dadosListaAnuncio.todasPaginas.data.length) {
+                return
+            }
+            setDadosListaAnuncio(prevState => {
+                return {
+                    ...prevState,
+                    paginacao: dadosListaAnuncio.todasPaginas.data.slice
+                        (dadosListaAnuncio.paginaRetorna + 25, dadosListaAnuncio.paginaAvanca + 25)//recorta a array e mostra somente 25 linhas
+                    , paginaAvanca: dadosListaAnuncio.paginaAvanca + 25,
+                    paginaRetorna: dadosListaAnuncio.paginaRetorna + 25, //soma os contadores
+                    paginaAtual: dadosListaAnuncio.paginaAtual += 1,
+                }
+            })
+        }
+        if (direcao === "retornar") {
+            if (dadosListaAnuncio.paginaRetorna <= 0) {
+                return
+            }
+            setDadosListaAnuncio(prevState => {
+                return {
+                    ...prevState,
+                    paginacao: dadosListaAnuncio.todasPaginas.data.slice
+                        (dadosListaAnuncio.paginaRetorna - 25, dadosListaAnuncio.paginaAvanca - 25)//recorta a array e mostra somente 25 linhas
+                    , paginaAvanca: dadosListaAnuncio.paginaAvanca - 25,
+                    paginaRetorna: dadosListaAnuncio.paginaRetorna - 25,
+                    paginaAtual: dadosListaAnuncio.paginaAtual -= 1 //soma os contadores
+                }
+            })
+        }
+
+    }
 
     return (
         <>
-            {dadosListaAnuncio &&
-                <h3 className="listaranuncio-h3">{dadosListaAnuncio.data.length} ANÚNCIOS</h3>
+            {dadosListaAnuncio.todasPaginas &&
+                <h3 className="listaranuncio-h3">{dadosListaAnuncio.todasPaginas.data.length} ANÚNCIOS</h3>
             }
             <table className="listaranuncio-table">
                 <tr className="listaranuncio-table-tr">
@@ -61,8 +114,8 @@ export default function ListarAnuncios() {
                 </th>
                 </tr>
 
-                {dadosListaAnuncio &&
-                    dadosListaAnuncio.data.map((dados, index) => {
+                {dadosListaAnuncio.todasPaginas &&
+                    dadosListaAnuncio.paginacao.map((dados, index) => {
                         return (
 
                             <tr className="listaranuncio-table-tr-tr">
@@ -96,8 +149,8 @@ export default function ListarAnuncios() {
                                 <td style={{ width: "4%" }} className="listaranuncio-table-td" key={index} >
                                     {dados.finalPlaca}
                                 </td>
-                                <td style={{ width: "4%" }} className="listaranuncio-table-td" key={index} >
-                                    {dados.ativado}
+                                <td style={{ width: "4%", backgroundColor: dados.ativado ? " rgb(157, 213, 131)" : "rgb(216, 114, 103)" }} className="listaranuncio-table-td" key={index} >
+                                    {dados.ativado ? "ATIVADO" : "DESATIVADO"}
                                 </td>
                                 <td style={{ width: "8%" }} className="listaranuncio-table-td" key={index} >
                                     {<SimpleModal dados={dados.id} />}
@@ -110,11 +163,20 @@ export default function ListarAnuncios() {
 
             <div className="listaranuncio-paginacao">
                 <div className="listaranuncio-paginacao-left">
-                    <i class="fas fa-arrow-circle-left fa-2x"></i>
+                    <i class="fas fa-arrow-circle-left fa-2x"
+                        onClick={(dadosClick, direcao = "retornar") => { FunctionPaginacao(dadosClick, direcao) }}
+                    ></i>
+                </div>
+
+                <div className="listaranuncio-paginacao-numeros">
+                    {dadosListaAnuncio.paginaAtual} de {dadosListaAnuncio.paginaFinal}
                 </div>
 
                 <div className="listaranuncio-paginacao-rigth">
-                    <i class="fas fa-arrow-circle-right fa-2x"></i>
+                    <i
+
+                        onClick={(dadosClick, direcao = "avancar") => { FunctionPaginacao(dadosClick, direcao) }}
+                        class="fas fa-arrow-circle-right fa-2x"></i>
                 </div>
             </div>
         </>
