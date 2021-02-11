0e367ba9e803f97e7f5fd6890f57b891b63f7027
MarcoVercosa <marco2007sky@hotmail.com>
2021-02-11 03:43:57 -0300

    Montagem formulario Ediçao em andamento !

--- server/index.js
+++ server/index.js
@@ -1,34 +1,27 @@
 const customExpress = require("./configExpress/customExpress")
-const conectaBDCarro = require("./infraBD/conexaoCarro")
-const conectaBDInformacoes = require("./infraBD/conexaoinformacoes")
+const conectaBDCarroAndInformacoes = require("./infraBD/conexaoCarro")
+
 
 
 const tabelas = require("./infraBD/tabelas")
 
 
-conectaBDCarro.connect((erro) => {
+conectaBDCarroAndInformacoes.connect((erro) => {
     if (erro) {
         console.log("Houve um erro para carregar o banco de dados => " + erro)
-        BDInformacaoes()
+
     }
     else {
-        tabelas.CriaCheckTabela(conectaBDCarro)
-        console.log("Conectado no banco de dados ===> CARROS <=== com sucesso")
-        BDInformacoes()
+        tabelas.CriaCheckTabela(conectaBDCarroAndInformacoes)
+        console.log("Conectado no banco de dados ===> CARROS e INFORMACOES <=== com sucesso")
+        const app = customExpress()
+        app.listen(9000, () => { (console.log("Servidor rodando na porta 9000")) })
+
+
+
     }
 
 })
 
-async function BDInformacoes() {
-    conectaBDInformacoes.connect(erro => {
-        if (erro) {
-            console.log("Houve um erro para carregar o banco de dados => " + erro)
-        } else {
-            console.log("Conectado no banco de dados ===> INFORMACOES <=== com sucesso")
-            tabelas.CkeckTabelaCarros(conectaBDInformacoes)
-            const app = customExpress()
-            app.listen(9000, () => { (console.log("Servidor rodando na porta 9000")) })
-        }
-    })
-}
+
 
