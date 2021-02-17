dbf666483f445919dca94c2ddb9c578d8f24831e
MarcoVercosa <marco2007sky@hotmail.com>
2021-02-17 03:36:07 -0300

    Montagem formulario Ediçao em andamento !

--- client/src/components/destaques/destaques.jsx
+++ client/src/components/destaques/destaques.jsx
@@ -4,6 +4,8 @@ import "./destaques.css"
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Button } from '@material-ui/core/';
 import SearchIcon from '@material-ui/icons/Search';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 import BuscaBD from "../fetchBackEnd/api"
 
@@ -19,21 +21,47 @@ export default function Destaques(dados) {
             },
             button: {
                 margin: theme.spacing(1),
+
             },
         },
     }));
     const classes = useStyles();
 
 
-    const [imagensDestaque, setImagensDestaque] = useState(false)
+    const [imagensDestaque, setImagensDestaque] = useState({
+
+        todosDestaques: false,
+        paginacao: 0,
+        paginaAvanca: 8,
+        paginaRetorna: 0,
+        ativaBotaoEsquerdo: false,
+        ativaBotaodireito: false
+
+
+    })
 
     useEffect(async () => {
         const classBuscaBD = new BuscaBD
         const resultado = await classBuscaBD.BuscaDestaque()
         console.log(resultado)
-        setImagensDestaque(resultado)
+        setImagensDestaque(prevState => {
+            return { ...prevState, todosDestaques: resultado.data, paginacao: resultado.data.slice(imagensDestaque.paginaRetorna, imagensDestaque.paginaAvanca) }
+        })
 
     }, [])
+
+    function Paginacao(direcao) {
+        if (direcao === "avancar") {
+            if (imagensDestaque.paginaAvanca >= imagensDestaque.todosDestaques.length) { return }
+            setImagensDestaque(prevState => {
+                return { ...prevState, paginacao: imagensDestaque.todosDestaques.slice(imagensDestaque.paginaRetorna + 8, imagensDestaque.paginaAvanca + 8), paginaAvanca: imagensDestaque.paginaAvanca + 8, paginaRetorna: imagensDestaque.paginaRetorna + 8 }
+            })
+        } else {
+            setImagensDestaque(prevState => {
+                return { ...prevState, paginacao: imagensDestaque.todosDestaques.slice(imagensDestaque.paginaRetorna - 8, imagensDestaque.paginaAvanca - 8), paginaAvanca: imagensDestaque.paginaAvanca - 8, paginaRetorna: imagensDestaque.paginaRetorna - 8 }
+            })
+        }
+    }
 
     return (
         <article className="destaques-article">
@@ -60,9 +88,9 @@ export default function Destaques(dados) {
             </div>
 
             <div className="destaques-article-div-grid">
-                {imagensDestaque &&
+                {imagensDestaque.paginacao &&
 
-                    imagensDestaque.data.map((recebe, index) => {
+                    imagensDestaque.paginacao.map((recebe, index) => {
                         const dados = { ...recebe, imagensPath: JSON.parse(recebe.imagensPath) }
                         return (
                             <div className="destaques-article-div-grid-div">
@@ -88,6 +116,35 @@ export default function Destaques(dados) {
                     })
 
                 }
+            </div>
+            <div classBuscaBD="destaques-article-div-navegacao">
+                <Button
+                    onClick={(click, direcao = "voltar") => {
+                        Paginacao(direcao)
+                    }}
+                    style={{ marginLeft: "20px" }}
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    startIcon={<ArrowBackIosIcon style={{ fontSize: 28, marginLeft: "20px" }} />}
+                    disabled={imagensDestaque.paginaRetorna <= 0}
+
+                >
+
+                </Button>
+                <Button
+                    onClick={(click, direcao = "avancar") => {
+                        Paginacao(direcao)
+                    }}
+                    style={{ marginLeft: "20px" }}
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    startIcon={<ArrowForwardIosIcon style={{ fontSize: 28, marginLeft: "20px" }} />}
+                    disabled={imagensDestaque.paginaAvanca >= imagensDestaque.todosDestaques.length}
+                >
+
+                </Button>
             </div>
 
         </article>
